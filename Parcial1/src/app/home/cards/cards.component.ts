c9b5cfccd319afd2bb942f02064c6867e415ded8
MyPortatil <adtellouden@gmail.com>
2025-10-01 17:11:18 -0500

    feat: Updates

--- Parcial1/src/app/home/cards/cards.component.ts
+++ Parcial1/src/app/home/cards/cards.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonComponent } from '../button/button.component';
+import { Image } from 'primeng/image';
 
 @Component({
   selector: 'app-cards',
-  imports: [CardModule, ButtonComponent],
+  imports: [CardModule, ButtonComponent, Image],
   standalone: true,
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
