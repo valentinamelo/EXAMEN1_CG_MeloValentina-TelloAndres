b8cc568702f063a730230faf44294b9fab096d65
MyPortatil <adtellouden@gmail.com>
2025-10-01 16:41:37 -0500

    feat: Finish Home

--- Parcial1/src/app/home/cards_3/cards.component.ts
+++ Parcial1/src/app/home/cards_3/cards.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-cards-3',
-  imports: [CardModule],
+  imports: [CardModule, ButtonComponent],
   standalone: true,
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
